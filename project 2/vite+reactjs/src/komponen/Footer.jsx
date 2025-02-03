67673b072ecaf631ba50ba88006c80ef7fa09637
faisal426 <github7251gmail.com>
2025-02-03 16:42:29 +0700

    vite+reactjs

---
+++ project 2/vite+reactjs/src/komponen/Footer.jsx
@@ -0,0 +1,12 @@
+import '../style/Footer.css'
+function Footer(){
+  return(
+<div id="copyright">
+        <div class="wrapper">
+            &copy; 2020. <b>RumahRafif.</b> All Rights Reserved.
+        </div>
+    </div>
+    )
+}
+
+export default Footer
